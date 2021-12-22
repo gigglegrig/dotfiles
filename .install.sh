a4dbf86fe39c494e6ee0cd747681dc0f8047c347
XI SUN <xisun@XIs-MacBook-Pro.local>
2021-12-22 03:40:42 -0500

    instlal.sh

--- .install.sh
+++ .install.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
+chsh -s /bin/bash
 # Assume you are on centos or redhat
 sudo yum -y install git
 # clone git repo and submodules
 git init
 git remote add origin https://github.com/gigglegrig/dotfiles.git
-git fetch
-git checkout -t origin/master -f
+git reset --hard origin/master
+git pull
 git submodule update --init --recursive
 
-# install vim 8
-sudo yum install -y ncurses-devel unzip
-wget https://github.com/vim/vim/archive/master.zip
-unzip ./master.zip
-cd vim-master/src/
-./configure --with-features=huge -enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config
-sudo make
-sudo make install
-export PATH=/usr/local/bin:$PATH
-cd ~
+# vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# base16
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
 
 
 source ~/.bashrc
 vim +PluginInstall +qall
+
